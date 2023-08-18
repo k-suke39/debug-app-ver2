835c4a2abea4cafbee69d9a3eb7740a4424eceea
k-suke39 <pz.mozarla@gmail.com>
2023-08-18 16:09:51 +0900

    🔥

--- db/migrate/20230818063123_create_users.rb
+++ db/migrate/20230818063123_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.integer :age
-      t.integer :weight
-      t.integer :height
+      t.string :name, null: false
+      t.integer :age, null: false
+      t.integer :weight, null: false
+      t.integer :height, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,10 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_18_063123) do
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.integer "weight"
-    t.integer "height"
+    t.string "name", null: false
+    t.integer "age", null: false
+    t.integer "weight", null: false
+    t.integer "height", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
